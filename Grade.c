7b94dd8d819203c36cc59a95fe8f56cb016222f0
YogBharati99 <57194032+YogBharati99@users.noreply.github.com>
2020-10-29 22:27:54 +0530

    Add files via upload

---
+++ Grade.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+void main()
+{
+	int a;
+	printf("Enter Marks : ");
+	scanf("%d",&a);
+	
+	if(a>=85 && a<=100)
+	{
+		printf("Grade A");
+		
+	}
+	
+	else if (a>=70 && a<=84)
+	{
+		printf("Grade B");
+	}
+	
+	else if (a>=55 && a<=69)
+	{
+		printf("Grade C");
+	}
+	
+	else if (a>=40 && a<=54)
+	{
+		printf("Grade D");
+	}
+	else if (a<40)
+	{
+		printf("Grade F");
+	}
+	
+}
